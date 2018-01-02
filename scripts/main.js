b261397d4d1042d9666a13e37f8e6d0c93dcaf2a
jqueguin <jqueguin@laptop826671.home>
2018-01-02 18:17:50 +0100

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,65 @@
+var tep = new TrelloExportPopup();
+
+var tui = new TrelloUI({
+        dispatchListsReady: true
+    });
+
+chrome.extension.sendMessage({}, function (response) {});
+
+document.addEventListener('trelloui-listsready', function() {
+    var popover = $(".pop-over");
+
+    tep.init();
+
+    $('.list-header-menu-icon').click(function (event) {
+        var popover_summoned_interval = setInterval(function () {
+            if ($(popover).is(':visible')) {
+                clearInterval(popover_summoned_interval);
+                $(".pop-over .content").append('<hr><ul class="pop-over-list"> <li><a class="js-export-list" href="#">Export This List</a></li> </ul>');
+                $(".js-export-list").click(function (e) {
+                    exportList(event);
+                });
+            }
+        }, 50);
+    });
+});
+
+function exportList(event) {
+    tep.hide();
+    var first_card_id = findFirstCardId(event);
+    if (!first_card_id) {
+        alert('No cards found in the list.');
+        return false;
+    }
+
+    chrome.extension.sendMessage({
+        command: 'getCardListId',
+        id: first_card_id
+    }, function (data) {
+        if (data.idList !== undefined) {
+            chrome.extension.sendMessage({
+                command: 'getListCards',
+                id: data.idList
+            }, function (data) {
+                tep.show(data);
+            });
+        }
+    });
+}
+
+
+/**
+ * Uses the menu button on a card to find the first card in that list and get its ID
+ * Returns false if not found, or the ID if there is a card
+ * @param event
+ * @returns bool | string
+ */
+function findFirstCardId(event) {
+    var titles = $(event.currentTarget).parent().parent().find('a.list-card-title:first');
+    if (titles[0] === undefined) {
+        console.error('List has no cards!');
+        return false;
+    } else {
+        return $(titles[0]).attr('href').split('/')[2];
+    }
+}
